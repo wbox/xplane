5232c9fb66f7fbd5b338913e700a74670a128dfc
Sam <sribas@gmail.com>
2023-10-08 22:38:03 -0400

    Update a350oilpsi.lua

--- ffa350/a350oilpsi.lua
+++ ffa350/a350oilpsi.lua
@@ -8,14 +8,28 @@
 -- does calculate the oil pressure properly. At this moment the values are obtained
 -- from both engine N3
 -- ----------------------------------------------------------------------------------
-
 if PLANE_ICAO == "A359" then
 
     function set_oil_psi()
-        set_array("sim/flightmodel/engine/ENGN_oil_press_psi", 0, get("1-sim/ind/eng/N3/L"))
-        set_array("sim/flightmodel/engine/ENGN_oil_press_psi", 1, get("1-sim/ind/eng/N3/R"))
+
+        local left_psi = 0
+        local right_psi = 0
+
+        DataRef( "engine_n3_left", "1-sim/ind/eng/N3/L" )
+        DataRef( "engine_n3_right", "1-sim/ind/eng/N3/R")
+
+        if engine_n3_left > 0 then
+            left_psi = math.floor((engine_n3_left / 0.62)/3)
+        end
+
+        if engine_n3_right > 0 then
+            right_psi = math.floor((engine_n3_right / 0.62)/3)
+        end
+        
+        set_array("sim/flightmodel/engine/ENGN_oil_press_psi", 0, left_psi)
+        set_array("sim/flightmodel/engine/ENGN_oil_press_psi", 1, right_psi)
     end
 
-    do_often("set_oil_psi()")
+    do_every_frame("set_oil_psi()")
 
 end
